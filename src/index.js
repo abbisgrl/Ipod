3a04c0897b2203f3cbf611eb1fa2701fccfe6e93
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-05-31 11:29:03 +0530

    final

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
-
+//this is the main starting of the server
 ReactDOM.render(
   <React.StrictMode>
     <App />
